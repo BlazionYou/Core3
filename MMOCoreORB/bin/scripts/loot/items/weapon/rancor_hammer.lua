2c2d3396acebc0d7e3a32d4b2d5252a08a52731d
BlazionYou <acarr89@gmail.com>
2026-03-17 01:27:28 -0400

    Update

--- MMOCoreORB/bin/scripts/loot/items/weapon/rancor_hammer.lua
+++ MMOCoreORB/bin/scripts/loot/items/weapon/rancor_hammer.lua
@@ -5,17 +5,17 @@ rancor_hammer = {
 	customObjectName = "Rancor Hammer",
 	directObjectTemplate = "object/weapon/melee/2h_sword/2h_sword_maul.iff",
 	craftingValues = {
-		{"mindamage", 400, 700, 0},
-		{"maxdamage", 800, 1400, 0},
+		{"mindamage", 400, 1700, 0},
+		{"maxdamage", 1800, 3400, 0},
 		{"attackspeed", 10.0, 6.0, 1},
 		{"woundchance", 35, 65, 1},
 		{"roundsused", 40, 80, 0},
 		{"hitpoints", 5000, 10000, 0},
 		{"zerorange", 0, 0, 0},
 		{"zerorangemod", 190, 290, 0},
-		{"midrange", 6, 6, 0},
+		{"midrange", 16, 16, 0},
 		{"midrangemod", 32, 132, 0},
-		{"maxrange", 18, 18, 0},
+		{"maxrange", 32, 32, 0},
 		{"maxrangemod", 0, 50, 0},
 		{"attackhealthcost", 40, 40, 0},
 		{"attackactioncost", 35, 35, 0},
@@ -25,8 +25,8 @@ rancor_hammer = {
 	customizationValues = {},
 	randomDotChance = 0,
 	junkDealerTypeNeeded = JUNKARMS,
-	junkMinValue = 10000,
-	junkMaxValue = 20000
+	junkMinValue = 100000,
+	junkMaxValue = 200000
 }
 
 addLootItemTemplate("rancor_hammer", rancor_hammer)
--- MMOCoreORB/bin/scripts/loot/items/weapon/rancor_t21.lua
+++ MMOCoreORB/bin/scripts/loot/items/weapon/rancor_t21.lua
@@ -5,18 +5,18 @@ rancor_t21 = {
 	customObjectName = "Rancor T21",
 	directObjectTemplate = "object/weapon/ranged/rifle/rifle_t21.iff",
 	craftingValues = {
-		{"mindamage", 180, 450, 0},
-		{"maxdamage", 420, 750, 0},
+		{"mindamage", 580, 1450, 0},
+		{"maxdamage", 1420, 2750, 0},
 		{"attackspeed", 10.0, 6.0, 1},
 		{"woundchance", 35, 65, 1},
 		{"roundsused", 40, 80, 0},
 		{"hitpoints", 10000, 10000, 0},
 		{"zerorange", 0, 0, 0},
 		{"zerorangemod", -90, 90, 0},
-		{"midrange", 60, 60, 0},
-		{"midrangemod", 32, 132, 0},
+		{"midrange", 84, 84, 0},
+		{"midrangemod", 132, 320, 0},
 		{"maxrange", 264, 264, 0},
-		{"maxrangemod", 0, 50, 0},
+		{"maxrangemod", 150, 150, 0},
 		{"attackhealthcost", 40, 40, 0},
 		{"attackactioncost", 35, 35, 0},
 		{"attackmindcost", 50, 50, 0},
@@ -25,8 +25,8 @@ rancor_t21 = {
 	customizationValues = {},
 	randomDotChance = 0,
 	junkDealerTypeNeeded = JUNKARMS,
-	junkMinValue = 10000,
-	junkMaxValue = 20000
+	junkMinValue = 100000,
+	junkMaxValue = 200000
 }
 
 addLootItemTemplate("rancor_t21", rancor_t21)
