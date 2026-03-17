a497728635858d9f4a8ee8ca9b83ac888c147a83
BlazionYou <acarr89@gmail.com>
2026-03-17 01:21:05 -0400

    Update

--- MMOCoreORB/bin/scripts/loot/items/weapon/rancor_hammer.lua
+++ MMOCoreORB/bin/scripts/loot/items/weapon/rancor_hammer.lua
@@ -3,14 +3,14 @@ rancor_hammer = {
 	minimumLevel = 0,
 	maximumLevel = -1,
 	customObjectName = "Rancor Hammer",
-	directObjectTemplate = "object/weapon/melee/polearm/lance_staff_metal.iff",
+	directObjectTemplate = "object/weapon/melee/2h_sword/2h_sword_maul.iff",
 	craftingValues = {
 		{"mindamage", 400, 700, 0},
 		{"maxdamage", 800, 1400, 0},
 		{"attackspeed", 10.0, 6.0, 1},
 		{"woundchance", 35, 65, 1},
 		{"roundsused", 40, 80, 0},
-		{"hitpoints", 10000, 10000, 0},
+		{"hitpoints", 5000, 10000, 0},
 		{"zerorange", 0, 0, 0},
 		{"zerorangemod", 190, 290, 0},
 		{"midrange", 6, 6, 0},
@@ -25,8 +25,8 @@ rancor_hammer = {
 	customizationValues = {},
 	randomDotChance = 0,
 	junkDealerTypeNeeded = JUNKARMS,
-	junkMinValue = 100,
-	junkMaxValue = 200
+	junkMinValue = 10000,
+	junkMaxValue = 20000
 }
 
 addLootItemTemplate("rancor_hammer", rancor_hammer)
