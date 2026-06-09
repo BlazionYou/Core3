37fe430900d93ebb2bbf1ea3af5a71c5c0e7fd0a
BlazionYou <acarr89@gmail.com>
2026-06-08 21:24:55 -0400

    Update

--- MMOCoreORB/bin/scripts/object/weapon/melee/sword/kinetic_saber/sword_kinetic_saber_gen1.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/sword/kinetic_saber/sword_kinetic_saber_gen1.lua
@@ -1,4 +1,6 @@
 
+
+
 object_weapon_melee_sword_kinetic_saber_sword_kinetic_saber_gen1 = object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen1:new {
 	playerRaces = {
 		"object/creature/player/bothan_male.iff",
@@ -23,19 +25,40 @@ object_weapon_melee_sword_kinetic_saber_sword_kinetic_saber_gen1 = object_weapon
 		"object/creature/player/zabrak_female.iff"
 	},
 
+	-- RANGEDATTACK, MELEEATTACK, FORCEATTACK, TRAPATTACK, GRENADEATTACK, HEAVYACIDBEAMATTACK,
+	-- HEAVYLIGHTNINGBEAMATTACK, HEAVYPARTICLEBEAMATTACK, HEAVYROCKETLAUNCHERATTACK, HEAVYLAUNCHERATTACK
 	attackType = MELEEATTACK,
+
+	-- ENERGY, KINETIC, ELECTRICITY, STUN, BLAST, HEAT, COLD, ACID, LIGHTSABER
 	damageType = KINETIC,
-	armorPiercing = MEDIUM,
 
-	xpType = "crafting_weapons_general",
+	-- NONE, LIGHT, MEDIUM, HEAVY
+	armorPiercing = HEAVY,
 
-	certificationsRequired = {},
+	-- combat_rangedspecialize_bactarifle, combat_rangedspecialize_rifle, combat_rangedspecialize_pistol, combat_rangedspecialize_heavy, combat_rangedspecialize_carbine
+	-- combat_meleespecialize_unarmed, combat_meleespecialize_twohand, combat_meleespecialize_polearm, combat_meleespecialize_onehand, combat_general,
+	-- combat_meleespecialize_twohandlightsaber, combat_meleespecialize_polearmlightsaber, jedi_general
+	xpType = "jedi_general",
+
+	-- See http://www.ocdsoft.com/files/certifications.xls
+	certificationsRequired = { "cert_onehandlightsaber_gen1" },
+	-- See http://www.ocdsoft.com/files/accuracy.xls
 	creatureAccuracyModifiers = { "onehandlightsaber_accuracy" },
+
+	-- See http://www.ocdsoft.com/files/defense.xls
 	defenderDefenseModifiers = { "melee_defense" },
+
+	-- Leave as "dodge" for now, may have additions later
 	defenderSecondaryDefenseModifiers = { "saber_block" },
+
+	-- See http://www.ocdsoft.com/files/speed.xls
 	speedModifiers = { "onehandlightsaber_speed" },
+
+	-- Leave blank for now
 	damageModifiers = { },
 
+
+	-- The values below are the default values.  To be used for blue frog objects primarily
 	healthAttackCost = 20,
 	actionAttackCost = 35,
 	mindAttackCost = 40,
@@ -64,6 +87,7 @@ object_weapon_melee_sword_kinetic_saber_sword_kinetic_saber_gen1 = object_weapon
 	childObjects = {
 		{templateFile = "object/tangible/inventory/lightsaber_inventory_1.iff", x = 0, z = 0, y = 0, ox = 0, oy = 0, oz = 0, ow = 0, cellid = -1, containmentType = 4}
 	},
+
 
 	numberExperimentalProperties = {1, 1, 2, 2, 2, 2, 2, 1, 1, 1},
 	experimentalProperties = {"XX", "XX", "CD", "OQ", "CD", "OQ", "CD", "OQ", "SR", "UT", "CD", "OQ", "OQ", "OQ", "OQ"},
