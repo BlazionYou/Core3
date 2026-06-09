e7f495dd71b3f8965b04d23b78540311d6ed124f
BlazionYou <acarr89@gmail.com>
2026-06-08 22:22:10 -0400

    Update

--- MMOCoreORB/bin/scripts/object/weapon/melee/sword/kinetic_saber/objects.lua
+++ MMOCoreORB/bin/scripts/object/weapon/melee/sword/kinetic_saber/objects.lua
@@ -7,11 +7,15 @@ object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_training = Sh
 }
 ObjectTemplates:addClientTemplate(object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_training, "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_training.iff")
 
+
+
 object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen1 = SharedWeaponObjectTemplate:new {
 	clientTemplateFileName = "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen1.iff",
 	clientObjectCRC = 0xECC5A843
 }
 ObjectTemplates:addClientTemplate(object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen1, "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen1.iff")
+
+
 
 object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen2 = SharedWeaponObjectTemplate:new {
 	clientTemplateFileName = "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen2.iff",
@@ -19,17 +23,23 @@ object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen2 = Shared
 }
 ObjectTemplates:addClientTemplate(object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen2, "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen2.iff")
 
+
+
 object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen3 = SharedWeaponObjectTemplate:new {
 	clientTemplateFileName = "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen3.iff",
 	clientObjectCRC = 0x5E2BE4DA
 }
 ObjectTemplates:addClientTemplate(object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen3, "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen3.iff")
 
+
+
 object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen4 = SharedWeaponObjectTemplate:new {
 	clientTemplateFileName = "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen4.iff",
 	clientObjectCRC = 0x17268357
 }
 ObjectTemplates:addClientTemplate(object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen4, "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen4.iff")
+
+
 
 object_weapon_melee_sword_kinetic_saber_shared_sword_kinetic_saber_gen5 = SharedWeaponObjectTemplate:new {
 	clientTemplateFileName = "object/weapon/melee/sword/kinetic_saber/shared_sword_kinetic_saber_gen5.iff",
